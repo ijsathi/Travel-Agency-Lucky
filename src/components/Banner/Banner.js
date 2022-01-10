46951e53bd77f30ab73ecb34dbc80081b2feb191
ijsathi <ijsathi2001rs@gmail.com>
2022-01-10 20:10:14 +0600

    responsive done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
                                 <img style={{ width: "100%", height: "385px" }} src="https://i.ibb.co/H4KgLtr/12-removebg-preview.png" className="d-block " alt="..." />
                             </div>
                             <div className="col-lg-7">
-                                <h1 className="animate__bounceInDown" style={{ fontSize: "50px" }} >Capture the scene on camera</h1>
+                                <h1 className="animate__bounceInDown"  >Capture the scene on camera</h1>
                                 <p className='animate__bounceOut'>Montenegro is a small country that is packed with surprises. Traveling there last year was unexpected as I wanted to look for a country that is outside of the Schengen</p>
                             </div>
                         </div>
@@ -21,7 +21,7 @@ const Banner = () => {
                     <div className="carousel-item active ">
                         <div className="row p-5  bg-gradient d-flex align-items-center" >
                             <div className="col-lg-7">
-                                <h1 className="animate__animated animate__bounce" style={{ fontSize: "50px" }} >Tour your dream destination with tourve. </h1>
+                                <h1 className="animate__animated animate__bounce"  >Tour your dream destination with tourve. </h1>
                                 <p  >Being one of the world’s most beautiful countries, Croatia welcomes thousands of tourists most especially during the summer season where a lot of people come to enjoy its Mediterranean coastline</p>
                             </div>
                             <div className="col-lg-5">
@@ -35,7 +35,7 @@ const Banner = () => {
                                 <img style={{ width: "120%", height: "395px" }} src="https://i.ibb.co/68hQWkz/asian-couple-beach-1150-6399-removebg-preview.png" className="d-block " alt="..." />
                             </div>
                             <div className="col-lg-7">
-                                <h1 className="animate__pulse" style={{ fontSize: "50px" }}>Enjoy your best Moment </h1>
+                                <h1 className="animate__pulse" >Enjoy your best Moment </h1>
                                 <p className='animate__bounceInUp' >One of the most popular places to visit if you’re traveling to Croatia is Hvar Island. Most especially during summer, Hvar Island is crowded with tourists from all over the</p>
                             </div>
                         </div>
